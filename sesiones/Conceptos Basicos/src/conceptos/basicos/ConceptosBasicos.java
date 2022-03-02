bc2e0ca7798f4f38b5c8b5c99b6244a0b97609a4
eduardo0514 <14.eduardo.watson@gmail.com>
2022-03-02 12:35:11 -0600

    Se agrega proyecto de conceptos basicos

---
+++ sesiones/Conceptos Basicos/src/conceptos/basicos/ConceptosBasicos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conceptos.basicos;
+
+/**
+ *
+ * @author lenovo
+ */
+public class ConceptosBasicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola otra vez");
+    }
+    
+}
